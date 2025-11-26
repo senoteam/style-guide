a3617f3995b8957453a216350f210b3752b93102
htmlin <10543563@qq.com>
2025-11-26 14:33:03 +0800

    feat(commitlint-config): add rule to disallow PascalCase in subject

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -2,5 +2,6 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 300],
+    'subject-case': [2, 'never', ['pascal-case']],
   },
 }
