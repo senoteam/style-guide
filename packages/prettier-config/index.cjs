31942131030c8adfdd7b34ead8066fadeb797cc8
htmlin <10543563@qq.com>
2026-03-11 21:39:20 +0800

    feat!: ESM-only

--- packages/prettier-config/index.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  quoteProps: 'consistent',
-  semi: false,
-  singleQuote: true,
-  importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '^[./](?!.*ss$).*$', 'ss$'],
-  importOrderSeparation: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
-}
